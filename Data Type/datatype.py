e1165b1588db8d8f52a4d325dc4f52797a520b1b
catur arpal perkasa <catur.arp@gmail.com>
2022-02-19 02:18:05 +0700

    Data Types

---
+++ Data Type/datatype.py
@@ -0,0 +1,22 @@
+# int
+from tkinter.tix import WINDOW
+
+
+a = 10
+b = 2
+print(a, "plus", b, "is ", a+b, "with type:", type(a+b))
+
+# string
+b = 1.2
+print(b, type(b))
+
+# imajiner
+a = 2j+1
+print(a, isinstance(a, complex))
+
+# string
+s = "wowed"
+print(s)
+ss = """wowww
+woww"""
+print(ss)
